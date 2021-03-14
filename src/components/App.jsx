33fc5b6791e5d2c7e12c77efc34bb881acfa6aac
nityo <nityo.git@nityo.com>
2021-03-14 18:46:06 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import "../styles/index.scss";
+import Recipes from "./Recipes";
+import sword from "../images/sword-32x32.png";
+import swordSvg from "../images/sword.svg";
+
+function App() {
+  return (
+    <>
+      <section className="hero"></section>
+      <main>
+        <h1>Oh hai, React</h1>
+      </main>
+      <img src={sword} alt="sword" width="250" />
+      <img src={swordSvg} alt="sword" width="250" />
+      <Recipes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Recipes.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const elvenShieldRecipe = {
+  leatherStripe: 2,
+  ironIngot: 1,
+  refinedMoonstone: 4,
+};
+
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 4,
+};
+
+console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
+
+import React from "react";
+
+function Recipes() {
+  const [recipe, setRecipe] = useState({});
+  return (
+    <div>
+      <h3>Current Recipr:</h3>
+      <button onClick={() => setRecipe(elvenShieldRecipe)}>
+        Elven Shield Recipe
+      </button>
+      <button onClick={() => setRecipe(elvenGauntletsRecipe)}>
+        Elven Gauntlet Recipe
+      </button>
+
+      <ul>
+        {Object.keys(recipe).map((material) => (
+          <li key={material}>
+            {material}: {recipe[material]}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Recipes;
