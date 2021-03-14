3c70e10637fc508bf4e7daee9963f32512eedd30
nityo <nityo.git@nityo.com>
2021-03-15 00:53:30 +0530

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <section className="hero"></section>
       <main>
-        <h1>Oh hai, React</h1>
+        <h1>Oh hai, ReactJs</h1>
       </main>
       <img src={sword} alt="sword" width="250" />
       <img src={swordSvg} alt="sword" width="250" />
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
 const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 let mode = "development";
 let target = "web";
+let babelPlugins = ["react-refresh/babel"];
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+];
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
   target = "browserslist";
+  babelPlugins = [];
+}
+if (process.env.SERVE) {
+  plugins.push(new ReactRefreshWebpackPlugin());
 }
 
 module.exports = {
   mode: mode,
   target: target,
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]",
@@ -51,18 +65,13 @@ module.exports = {
               "@babel/preset-env",
               ["@babel/preset-react", { runtime: "automatic" }],
             ],
+            plugins: babelPlugins,
           },
         },
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  plugins: plugins,
   resolve: {
     extensions: [".js", ".jsx"],
   },
